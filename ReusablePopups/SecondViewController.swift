da5d11f10eba58bf8655f1b22fab0162820681e5
Brandon Mahoney <willb31681@gmail.com>
2019-03-04 14:54:39 -0600

    Initial Commit

---
+++ ReusablePopups/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ReusablePopups
+//
+//  Created by Brandon Mahoney on 3/4/19.
+//  Copyright © 2019 Brandon Mahoney. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
